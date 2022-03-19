22bfde44f147ac836e9036edf9737a5fe7c839cf
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-03-19 11:59:47 +0000

    Add files via upload

--- shm.c
+++ shm.c
@@ -6,7 +6,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <signal.h>
 #include "dxwindow.h"
+void handler(int value){
+	exit(1);
+}
+void onexits(int i,void *arg){
+	int n;
+	for(n=0;n<wcount;n++){
+		if(n<wcount)win[n].shms[flagend]=1;
+	}
+	sleep(4);
+	endX(fbfd);
+	puts("exit windows gest");
+	exitWindow();
+}
 int main(){
 	int ww1=-1;
 	char application[80]="./shm2 ";
@@ -17,11 +31,13 @@ int main(){
     char *ccc;
     int n=0;
 	int ii;
+	signal(SIGINT,handler);
 	startwin();
 	for (n=0;n<4;n++){
 		ww1=newWindow(ddhmp,n*20,n*20,winsw,winsh,100,100,100,aapplication);
 	}
 				if(ww1!=-1){
+					on_exit(onexits,(void *)0);
 					while(1){
 						if(win[0].shms[flagrefresh]==1 && win[1].shms[flagrefresh]==1 && win[2].shms[flagrefresh]==1 && win[3].shms[flagrefresh]==1)break;
 					}
@@ -34,15 +50,6 @@ int main(){
 						windowsRefresh();
 					}
 	}
-					win[0].shms[flagend]=1;
-					win[1].shms[flagend]=1;
-					win[2].shms[flagend]=1;
-					win[3].shms[flagend]=1;
-					sleep(4);
 				}
-	
-	endX(fbfd);
-	puts("exit windows gest");
-	exitWindow();
 	return 0;
 }
--- shm2.c
+++ shm2.c
@@ -6,16 +6,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <signal.h>
 #include "dxwindow.h"
+int *shm1;
+char *argvs;
+void handler(int value){
+	exit(1);
+}
+void onexits(int i,void *arg){
+	printf("on exit %s\n",argvs);
+	shm_unlink(argvs);
+}
 int main(int argc,char *argv[]){
 	struct windows w1;
 	char bufs[2080];
 	if (argc<2)exit(0);
+	argvs=(char*)argv[1];
+	signal(SIGINT,handler);
 	int fd = shm_open(argv[1],O_RDWR ,0);
 	if (fd==-1)exit(1);
 	if (ftruncate(fd,(winsw*winsh)*(sizeof(int))+50)==-1)exit(1);
-	int *shm1=(int*)mmap(NULL,(winsw*winsh)*(sizeof(int))+50,PROT_WRITE | PROT_READ ,MAP_SHARED,fd,0);
+	shm1=(int*)mmap(NULL,(winsw*winsh)*(sizeof(int))+50,PROT_WRITE | PROT_READ ,MAP_SHARED,fd,0);
 	if (shm1!=MAP_FAILED){
+		on_exit(onexits,(void *)0);
 		w1.dc=shm1+shmhead;
 		iline(0,0,winsw,winsh,w1.dc,0,0,0);
 		iline(0,winsh,winsw,0,w1.dc,0,0,0);
@@ -24,7 +37,5 @@ int main(int argc,char *argv[]){
 			if(shm1[flagend]==1)break;
 		}
 	}
-	printf("%s\n",argv[1]);
-	shm_unlink(shmp);
 	return 0;
 }
