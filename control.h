f948ef82ed38f111baa6296f0b92faf1a7d74d94
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-03-22 18:44:08 +0000

    Add files via upload

--- control.h
+++ control.h
@@ -1,7 +1,27 @@
 #include "dxwindow.h"
 #define flagoutput 4
 #define flaginput 10
+#define maxlabels 20
+int ccount;
+label lll[maxlabels];
 
+int addsControls(int x,int y,int w,int h,char *caption,int r,int g,int b){
+	int cccount=-1;
+	if (ccount<maxlabels){
+		lll[ccount].c.x=x;
+		lll[ccount].c.y=y;
+		lll[ccount].c.w=w;
+		lll[ccount].c.h=h;
+		strcpy(lll[ccount].caption,caption);
+		lll[ccount].r=r;
+		lll[ccount].g=g;
+		lll[ccount].b=b;
+		cccount=ccount;
+		ccount++;
+		return cccount;
+	}
+	return cccount;
+}
 void drawLabels(label l,int *dc){
 	char caption[100];
 	if(l.c.h<16)l.c.h=16;
@@ -14,9 +34,22 @@ void drawLabels(label l,int *dc){
 	caption[tl]='\0';
 	igputs(dc,l.c.x+3,l.c.y+3,0,0,0,caption);
 }
+void drawLabelsControls(int *dc){
+	int n;
+	for(n=0;n<ccount;n++)drawLabels(lll[n],dc);
+}
 int insides(label l1,int x,int y){
 	if(x>l1.c.x && y>l1.c.y && x<l1.c.x+l1.c.w && y<l1.c.y+l1.c.h){
 		return 1;
 	}
 	return 0;
+}
+int scanInsides(int x,int y){
+	int n;
+	int i;
+	for(n=0;n<ccount;n++){
+		i=insides(lll[n],x,y);
+		if (i==1)return n;
+	}
+	return -1;
 }
--- shm2.c
+++ shm2.c
@@ -21,18 +21,13 @@ int main(int argc,char *argv[]){
 	int i;
 	int xx;
 	int yy;
-	label l1;
+	char caption[80]="Exit";
+	char *captions=caption;
 	struct windows w1;
 	char bufs[2080];
-	l1.c.x=25;
-	l1.c.y=50;
-	l1.c.h=16;
-	l1.c.w=45;
-	l1.r=100;
-	l1.g=100;
-	l1.b=100;
-	strcpy(l1.caption,"Exit");
 	if (argc<2)exit(0);
+	addsControls(25,50,45,16,captions,100,100,100);
+	addsControls(80,50,45,16,captions,100,100,100);
 	argvs=(char*)argv[1];
 	signal(SIGINT,handler);
 	int fd = shm_open(argv[1],O_RDWR ,0);
@@ -42,7 +37,7 @@ int main(int argc,char *argv[]){
 	if (shm1!=MAP_FAILED){
 		on_exit(onexits,(void *)0);
 		w1.dc=shm1+shmhead;
-		drawLabels(l1,w1.dc);
+		drawLabelsControls(w1.dc);
 		shm1[flagoutput]=-1;
 		shm1[flagrefresh]=1;
 		while(1){
@@ -51,7 +46,7 @@ int main(int argc,char *argv[]){
 				xx=shm1[flagoutput+1];
 				yy=shm1[flagoutput+2];
 				shm1[flagoutput]=-1;
-				if(insides(l1,xx,yy)==1){
+				if(scanInsides(xx,yy)!=-1){
 					shm1[flaginput]=0;
 				}
 
